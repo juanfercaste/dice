104b6ac2f83dc2340c99d4c262c0546cbd950d91
Juan Fernando Cerrato Castellon <45551201+juanfercaste@users.noreply.github.com>
2019-11-01 07:06:04 -0600

    JavaScrip Changes
    Adding a button

--- behavior.js
+++ behavior.js
@@ -1,13 +1,12 @@
-var diceNumber1 = Math.floor(Math.random() * 6) + 1;
-var diceNumber2 = Math.floor(Math.random()* 6) + 1;
+var buttonRoll = document.getElementById("button");
 
+function rollDice(){
+var diceNumber1 = Math.floor(Math.random()* 6) + 1;
+var diceNumber2 = Math.floor(Math.random()* 6) + 1;
 var diceImage = "images/dice" + diceNumber1 +".png";
 var diceImage2 = "images/dice" + diceNumber2 + ".png";
-
-
 document.querySelectorAll("img")[0].setAttribute("src",diceImage);
 document.querySelectorAll("img")[1].setAttribute("src",diceImage2);
-
 
 if ( diceNumber1 > diceNumber2){
   document.querySelector("h1").innerHTML = "The First Player Wins";
@@ -18,3 +17,6 @@ else if (diceNumber2 > diceNumber1){
 else {
   document.querySelector("h1").innerHTML=" Oh No It's a draw";
 }
+}
+
+buttonRoll.addEventListener("click", rollDice);
